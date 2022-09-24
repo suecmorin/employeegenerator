3323edb46f5c000def17000f0052558dcf27b073
suecmorin <suecmorin@gmail.com>
2022-09-24 15:58:08 -0700

    finished index.js

---
+++ README.md
@@ -0,0 +1,14 @@
+PROJECT NAME EMPLOYEE TEAM GENERATOR
+
+DESCRIPTION This project uses node, jest and inquirer to generate a HTML application which
+allows the user to add engineers and interns to the team. Project includes test files whih use jest.
+
+INSTALLATION No installation required, as package JSON files are already created.
+
+USAGE User types 'node index.js'.
+
+LICENSE None
+
+CONTRIBUTING Sue Morin, YouTube
+
+CONTACT suecmorin@gmail.com github.com/suecmorin
---
+++ index.js
@@ -0,0 +1,235 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
+
+const createHTML = require("./dist/generateHTML");
+
+const managerTeam = [];
+
+function addManager() {
+console.log("Let's build your team. Please answer the following questions.");
+inquirer.prompt([
+    {
+        type: "input", 
+        message: "Enter your name:",
+        name: "managerName",
+        validate: managerName => {
+            if(managerName){
+                return true;
+            }else{
+                console.log("Name cannot be blank");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        message: "Enter your employee ID number:",
+        name: "managerID",
+        validate: managerID => {
+            if(managerID){
+                return true;
+            }else{
+                console.log("Please enter an ID number");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        message: "Enter your email:",
+        name: "managerEmail",
+        validate: managerEmail =>{
+            if(managerEmail){
+                return true;
+            }else{
+                console.log("Please enter an email address");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        message: "Enter your office number:",
+        name: "officeNumber",
+        validate: officeNumber =>{
+            if(officeNumber){
+                return true;
+            }else{
+                console.log("Please enter an office number");
+                return false
+            }
+        }
+    }
+ ]).then (data =>{
+     const manager = new Manager(data.managerName, data.managerID, data.managerEmail, data.officeNumber);
+     managerTeam.push(manager);
+
+addteamMember();
+})
+};
+
+function addteamMember() {
+    return inquirer.prompt([
+        {
+        type: "list",
+        message: "Enter employee's position:",
+        name: "employeeType",
+        choices: ["Engineer", "Intern"]
+        }
+    ]).then (function(answers){
+        switch(answers.employeeType){
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+        }
+    });
+};
+function addEngineer() {
+    return inquirer.prompt([
+        {
+        type: "input",
+        message: "Enter engineer's name:",
+        name: "engineerName",
+        validate: engineerName => {
+            if(engineerName){
+                return true;
+            }else{
+                console.log("Name cannot be blank");
+                return false;
+            }
+        }
+        },
+        {
+        type: "input",
+        message: "Enter engineer's ID number",
+        name: "engineerId",
+        validate: engineerId => {
+            if(engineerId){
+                return true;
+            }else{
+                console.log("Engineer ID cannot be blank");
+                return false;
+            }
+        }
+        },
+        {
+        type: "input",
+        message: "Enter engineer's email address",
+        name: "engineerEmail",
+        validate: engineerEmail => {
+            if(engineerEmail){
+                return true;
+            }else{
+                console.log("Email cannot be blank");
+                return false;
+            }
+        }
+        },
+        {
+        type: "input",
+        message: "Enter engineer's github account",
+        name: "engineerGithub",  
+        validate: engineerGithub => {
+            if(managerName){
+                return true;
+            }else{
+                console.log("Github account cannot be blank");
+                return false;
+            }
+        }
+        }
+    ]).then (data =>{
+        const engineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, data.engineerGithub);
+        managerTeam.push(engineer);
+        menu();
+    });
+};
+    function addIntern() {
+        return inquirer.prompt([
+            {
+            type: "input",
+            message: "Enter intern's name:",
+            name: "internName",
+            validate: internName => {
+                if(internName){
+                    return true;
+                }else{
+                    console.log("Name cannot be blank");
+                    return false;
+                }
+            }
+            },
+            {
+            type: "input",
+            message: "Enter intern's ID number",
+            name: "internId",
+            validate: internId => {
+                if(internId){
+                    return true;
+                }else{
+                    console.log("ID cannot be blank");
+                    return false;
+                }
+            }
+            },
+            {
+            type: "input",
+            message: "Enter intern's email address",
+            name: "internEmail",
+            validate: internEmail => {
+                if(internEmail){
+                    return true;
+                }else{
+                    console.log("Email cannot be blank");
+                    return false;
+                }
+            }
+            },
+            {
+            type: "input",
+            message: "Enter intern's college/university",
+            name: "internSchool",  
+            validate: internSchool => {
+                if(internSchool){
+                    return true;
+                }else{
+                    console.log("College/university cannot be blank");
+                    return false;
+                }
+            }
+            }
+        ]).then (data =>{
+            const intern = new Intern(data.internName, data.internId, data.internEmail, data.internSchool);
+            managerTeam.push(intern); 
+            menu();
+
+        });
+};
+function menu() {
+    return inquirer.prompt([
+        {
+        type: "list",
+        message: "Do you want to add another employee?",
+        name: "addEmployee",
+        choices: ["Yes", "No"]
+        }  
+    ]).then (function(answers){
+            switch(answers.addEmployee){
+                case "Yes":
+                    addteamMember();
+                    break;
+                case "No":
+                    buildTeam();
+                    break;
+            }
+        });
+
+};
+
